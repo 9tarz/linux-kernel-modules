31c115a9edadd991b8b93d40e5cea13d11df34ad
root <kanatip.jit@gmail.com>
2015-12-03 03:48:49 -0800

    simple linux kernel module

---
+++ simple/simple.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int simple_init(void)
+{
+  printk(KERN_INFO "Loading Module\n");
+  return 0;
+}
+
+void simple_exit(void)
+{
+  printk(KERN_INFO "Removing Module\n");
+}
+
+module_init(simple_init);
+module_exit(simple_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
+
