e3aec87f5bc5a03b672863148c1149486f7e7625
root <kanatip.jit@gmail.com>
2015-12-03 04:28:11 -0800

    Listing tasks kernel module

---
+++ listing-task/listing-task.c
@@ -0,0 +1,30 @@
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kanatip Chitavisutthivong");
+MODULE_DESCRIPTION("Show all tasks kernel module");
+MODULE_VERSION("0.1");
+
+
+int listing_tasks_init(void)
+{
+	struct task_struct *task;
+
+	for_each_process(task) {
+		printk(KERN_INFO "[pid: %d] pname: %s [state: %ld]\n", task->pid, task->comm, task->state);
+	}
+	return 0;
+}
+
+void listing_tasks_exit(void)
+{
+	printk(KERN_INFO "Removing module\n");
+}
+
+module_init(listing_tasks_init);
+module_exit(listing_tasks_exit);
+
